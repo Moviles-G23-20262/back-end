2f3b45858f3110001962ee93520de4dc2458a47c
andyy2024 <a.boteror@uniandes.edu.co>
2026-09-20 09:38:51 -0500

    malditos bugs de mierda

--- src/chatrooms/dto/update-chatroom.dto.ts
+++ src/chatrooms/dto/update-chatroom.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateChatroomDto } from './create-chatroom.dto';
 
 export class UpdateChatroomDto extends PartialType(CreateChatroomDto) {}
